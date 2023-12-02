572d095b9e61e9eed976930d3b61a0da91c9ff47
Julian Huarachi <julian.huarachi@onesolutionsweb.com>
2023-12-02 17:25:06 -0400

    push

--- w11 arch dual/1.preinstall.sh
+++ w11 arch dual/1.preinstall.sh
@@ -1,103 +1,85 @@
 #!/bin/bash
 
+# Color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 function formatDisk {
+    clear
     
-    echo "=====> formateamos particion root"
+    echo -e "${BLUE} FORMAT ROOT PARTITION ${NC}"
     mkfs.ext4 -F -L "root" /dev/sdb4
-    echo ""
+    echo -e "${GREEN} finiched ${NC}"
     
-    # echo "formateamos particion home"
+    # echo -e "FORMAT HOME PARTITION"
     # mkfs.ext4 -F -L "home" /dev/sdb7
-    # echo ""
+    # echo -e "${GREEN} finiched ${NC}"
     
-    echo "=====> formateamos particion swap"
+    echo -e "${BLUE} FORMAT SWAP PARTITION ${NC}"
     mkswap -L "SWAP" /dev/sdb5
-    echo ""
-    
+    echo -e "${GREEN} finiched ${NC}"
 }
 
 function prepareInstalation {
+    clear
     
-    echo "Montamos la raiz"
+    echo -e "${BLUE} MOUNT ROOT ${NC}"
     mount /dev/sdb4 /mnt
-    echo "raiz montadada en /mnt"
-    echo ""
+    echo -e "${GREEN} finiched ${NC}"
     
-    echo "creamon directorio home"
+    echo -e "${BLUE} CREATE HOME DIRECTORY ${NC}"
     mkdir -p /mnt/home
-    echo "directorio home creada en /mnt/home"
-    echo ""
+    echo -e "${GREEN} finiched ${NC}"
     
-    # echo "montamos home"
+    # echo -e "montamos home"
     # mount /dev/sdb7 /mnt/home
-    # echo "directorio home montado en /mnt/home"
-    # echo ""
+    # echo -e "directorio home montado en /mnt/home"
+    # echo -e "${GREEN} finiched ${NC}"
     
-    echo "activamos swap"
+    echo -e "${BLUE} ACTIVATE SWAP ${NC}"
     swapon /dev/sdb5
-    echo "swap activado"
-    echo ""
+    echo -e "${GREEN} finiched ${NC}"
     
-    echo "creamos directorio efi"
+    echo -e "${BLUE} CREATE DIRECTORY EFI ${NC}"
     mkdir -p /mnt/boot/efi
-    echo "directorio efi creado en /mnt/boot/efi"
-    echo ""
+    echo -e "${GREEN} finiched ${NC}"
     
-    echo "montamos efi"
+    echo -e "${BLUE} MOUNT EFI ${NC}"
     mount /dev/sdb1 /mnt/boot/efi
-    echo "efi montado en /mnt/boot/efi"
-    echo ""
-    
-}
-
-function datetime {
-    echo "configuracion de hora y fecha"
-    timedatectl status
-    
-    # listar zonas horarias
-    # timedatectl list-timezones
-    
-    echo "change timezone"
-    timedatectl set-timezone America/La_Paz
-    
-}
-
-function fastrepository {
-    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-    pacman -Sy
-    pacman -S pacman-contrib --noconfirm
-    rankmirrors -n 10 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
+    echo -e "${GREEN} finiched ${NC}"
 }
 
 function installBase {
-    echo "INSTALACION DE SISTEMA BASE"
-    echo ""
-    
+    clear
+
+    echo -e "${BLUE} INSTALL SYSTEM BASE ${NC}"
     pacstrap -i /mnt base base-devel linux linux-firmware linux-headers dhcpcd gvfs-mtp gvfs-afc gvfs-gphoto2 vim git netctl dialog tmux intel-ucode sudo networkmanager nano
-    echo "instalacion terminada con exito"
+    echo -e "${GREEN} finiched ${NC}"
 }
 
 function configSystem {
-    echo "CONFIGURACION DEL SISTEMA"
-    echo ""
-    
-    echo "generar fichero fstab"
+    clear
+
+    echo -e "${BLUE} GEN fstab file ${NC}"    
     genfstab -U /mnt >> /mnt/etc/fstab
-    echo "fstab generado en /mnt/etc/fstab"
-    echo ""
+    echo -e "${GREEN} finiched ${NC}"
     
-    # echo "comprobamos que el fichero fue creado correctamente"
+    # echo -e "comprobamos que el fichero fue creado correctamente"
     # cat /mnt/etc/fstab
-    # echo ""
+    # echo -e "${GREEN} finiched ${NC}"
 }
 
 formatDisk
 prepareInstalation
-datetime
-# fastrepository
 installBase
 configSystem
-cp preinstall.sh /mnt/root/
+cp 2.install.sh /mnt/root/
+cp 3.postinstall.sh /mnt/root/
+cp 4.i3.sh /mnt/root/
+cp 5.utilities.sh /mnt/root/
 arch-chroot /mnt/
 swapoff -a
 umount -R /mnt
